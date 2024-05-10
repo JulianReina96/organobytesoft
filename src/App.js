2b5878441ce93b625159fa869b63cb54a7174521
Julian Reina <julianreina@gmail.com>
2024-05-10 20:51:14 -0300

    --add single page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import CampoTexto from './components/CampoTexto';
+import Formulario from './components/Formulario';
+import Banner from './components/banner';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<Banner></Banner>
+<Formulario></Formulario>
+
+
     </div>
   );
 }
